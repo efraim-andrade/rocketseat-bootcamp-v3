9f74bc6a69b6993a3a8a4e57c73f58514659dfc8
Efraim Andrade <efraim.morais@fatecitapetininga.edu.br>
2018-12-09 15:39:31 -0200

    Fazendo o gonode modulo2,
    aula cadastro de usuário.

--- gonode/modulo2/src/routes.js
+++ gonode/modulo2/src/routes.js
@@ -2,8 +2,9 @@ const express = require('express')
 
 const routes = express.Router()
 
-routes.get('/', (req, res) => {
-  return res.send('Hello World')
-})
+const UserController = require('./app/controllers/UserController')
+
+routes.get('/signup', UserController.create)
+routes.post('/signup', UserController.store)
 
 module.exports = routes
