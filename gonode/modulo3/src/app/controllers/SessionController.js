fe8c38515186fc4b8d06560e1a4d1260ecfc88bf
Efraim Andrade <efraim.morais@fatecitapetininga.edu.br>
2018-12-15 08:51:48 -0200

    fazendo gonode modulo3,
    configurando mongoDB, mongoose, schema de usuario, cadastro e insomnia, criptografia de senha, autenticacao jwt

---
+++ gonode/modulo3/src/app/controllers/SessionController.js
@@ -0,0 +1,22 @@
+const User = require('../models/Users')
+
+class SessionController {
+  async store (req, res) {
+    const { email, password } = req.body
+
+    const user = await User.findOne({ email })
+
+    if (!user) {
+      return res.status(404)
+        .json({ error: 'Usuario invalido' })
+    }
+
+    if (!await user.compareHash(password)) {
+      return res.status(400).json({ error: 'Senha invalida' })
+    }
+
+    return res.json({ user, token: User.generateToken(user) })
+  }
+}
+
+module.exports = new SessionController()
---
+++ gonode/modulo3/src/app/controllers/UserController.js
@@ -0,0 +1,18 @@
+const User = require('../models/Users')
+
+class UserController {
+  async store (req, res) {
+    const { email } = req.body
+
+    if (await User.findOne({ email })) {
+      return res.status(400)
+        .json({ error: 'Usuario ja existe' })
+    }
+
+    const user = await User.create(req.body)
+
+    return res.json(user)
+  }
+}
+
+module.exports = new UserController()
