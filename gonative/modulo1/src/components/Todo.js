c8d17067721a60adbbf04154e480645b0873c68b
Efraim Andrade <efraim.morais@fatecitapetininga.edu.br>
2018-12-27 21:29:30 -0200

    feat(GoNative - modulo 1): React - aula
    iniciando estrutura com react native

---
+++ gonative/modulo1/src/components/Todo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { View, Text, StyleSheet } from 'react-native';
+
+class Todo extends React.Component {
+    state = {}
+
+    render() {
+      return (
+        <View />
+      );
+    }
+}
+
+export default Todo;
---
+++ gonative/modulo1/src/index.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Hello World!!</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#333',
+  },
+  text: {
+    color: '#FFF',
+  }
+});
