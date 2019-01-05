95b025c01eb9a7d343ea947c39eaf67ac51e627e
Efraim Andrade <efraim.morais@fatecitapetininga.edu.br>
2019-01-05 18:35:23 -0200

    feat(GoNode - modulo 4): Node - aula
    configurando sentry, lidando com cors, salvando relacionamentos, utilizando transactions

--- gonode/modulo4/app/Controllers/Http/UserController.js
+++ gonode/modulo4/app/Controllers/Http/UserController.js
@@ -1,12 +1,19 @@
 'use strict'
 
+const Database = use('Database')
 const User = use('App/Models/User')
 
 class UserController {
   async store ({ request }) {
-    const data = request.only(['username', 'email', 'password'])
+    const data = request.only(['username', 'email', 'password']) // apenas os listados
+    const addresses = request.input('addresses') // Busca apenas o campo
 
-    const user = await User.create(data)
+    const trx = await Database.beginTransaction()
+
+    const user = await User.create(data, trx)
+    await user.addresses().createMany(addresses, trx) // cria os enderecos para o usuario
+
+    await trx.commit()
 
     return user
   }
