a7952bc8cbf4dd92449ea2ddd57288506c795864
Efraim Andrade <efraim.morais@fatecitapetininga.edu.br>
2019-01-10 03:01:03 -0200

    feat(GoReact - modulo 4): React - aula
    estruturando projeto

---
+++ goreact/modulo4/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import GlobalStyle from './styles/global';
+
+const App = () => (
+  <React.Fragment>
+    <GlobalStyle />
+
+    <h1>teste</h1>
+  </React.Fragment>
+);
+
+export default App;
